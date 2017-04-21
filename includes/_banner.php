caa3b9bce485fe52d3adcc9be2ae1a9a0235d356
Rajkumar S R <rajkumar@near.co>
2017-04-21 23:47:31 +0530

    Updated includes files and fixed some bugs in homepage

---
+++ includes/_banner.php
@@ -0,0 +1,21 @@
+<div id="particles-js" class="col-lg-12 brj k" style="background-image:url('<?= IMAGE ?>banner.jpeg'); ">
+	<div class="anim-imag" style="background-image:url('<?= IMAGE ?>anim-imag1.png');"></div>
+		<div class="container" style="height: 150px;"></div>
+		<div class="container banner-content">
+		<div><span>The platform processing</span></div>
+		<div><span class="red">Massive Data</span></div>
+		<div><span>to understand consumers</span></div>
+		<div><span>in Smart Environments</span></div>
+		<div class="line_break"></div>
+			<span class="line-2">"Location as a Single Identifier to combine 
+            <div id="typed-strings" class="type">
+                <span>Wifi</span>
+                <span>Location</span>
+                <span>Ambient Intelligents Platform</span>
+            </div>
+            <span id="typed" style="white-space:pre;"></span>"</span>
+            <div style="clear: both"></div>
+			<a href="#" class=" hp-cta-video " style="margin-top: 20px;">Play Video</a>
+		</div>
+		<div class="mouse"></div>
+	</div>
---
+++ includes/_footer.php
@@ -0,0 +1,92 @@
+<footer style="padding:15px; background:#f5f5f5;">
+			<div class="container spacing_margin">
+				<div class="row" >
+				   <div style="width:100%; margin-left:15px;" data-aos="fade-up" data-aos-delay="200">
+				   <span style="width:102px; display:block;">
+					   <svg version="1.1" x="0px" y="0px" viewBox="0 0 491.1 145.6" xml:space="preserve">
+        <style type="text/css">
+            .near-logo{fill:#5C5C5C;}
+            .a-logo{fill:#ED1C24;}
+        </style>
+        <polygon class="near-logo" points="447.4,126.7 451,126.7 451,136.4 452.6,136.4 452.6,126.7 456.2,126.7 456.2,125.2 447.4,125.2 "></polygon>
+        <polygon class="near-logo" points="463.6,133.7 463.5,133.7 459.9,125.2 457.7,125.2 457.7,136.4 459.2,136.4 459.2,127.2 459.2,127.2 
+                 463,136.4 464,136.4 467.8,127.2 467.8,127.2 467.8,136.4 469.4,136.4 469.4,125.2 467.1,125.2 "></polygon>
+        <path class="a-logo" d="M298.8,74.8L298.8,74.8c0-20.9,16.9-37.8,37.8-37.8c20.9,0,37.8,16.9,37.8,37.8c0,20.9-16.9,37.7-37.8,37.7
+              C315.7,112.6,298.8,95.7,298.8,74.8 M398,74.8c0-33.8-27.5-61.3-61.4-61.3c-33.8,0-61.3,27.4-61.3,61.3c0,33.8,27.4,61.3,61.3,61.3
+              h42.7c19.4,0,18.6-19.6,18.6-19.6V74.8z"></path>
+        <path class="near-logo" d="M107.5,126.2c0,5.4,4.8,10.2,11.8,10.2c6.6,0,11.8-5,11.8-11.6V75c0-33.8-27.4-61.3-61.3-61.3
+              C36.1,13.7,8.7,41.2,8.7,75v51.2c0,5.4,4.8,10.2,11.8,10.2c6.6,0,11.8-5,11.8-11.6V75h0c0-20.8,16.8-37.6,37.6-37.6
+              c20.8,0,37.6,16.8,37.6,37.6h0V126.2z"></path>
+        <path class="near-logo" d="M237.2,64.7h-33.5C198,64.7,193,69.3,193,75c0,5.7,4.9,10.2,10.6,10.2H249c4,0,7.5-2.1,10-5.2
+              c0,0,10-13.4,0.3-31.8c-9.9-20.4-31.5-34.5-55.7-34.5C169.8,13.7,143,41.1,143,75c0,33.8,26.8,61.2,60.6,61.2
+              c16.6,0,32.3-6,43.4-16.8h0c4.6-4.6,4.6-12,0-16.6c-4.5-4.5-11.7-4.6-16.3-0.2l0,0c-6.8,6.6-16.7,10.3-27,10.3
+              c-20.9,0-37.1-17.1-37.1-38s16.3-37.8,37.1-37.8c26.4,0,36,21.8,36,23.6C239.6,60.8,240.2,64.7,237.2,64.7"></path>
+        <path class="near-logo" d="M471.3,13.6c-14.3,0-27.3,3.7-37.7,11.9c0-6.5-5.4-11.8-11.9-11.8c-6.5,0-11.7,5.2-11.8,11.6h0l0,49.5h0
+              c0,0.1,0,0.3,0,0.4v50.9c0,5.4,4.8,10.3,11.9,10.3c6.6,0,11.9-5,11.9-11.7l0-49.5c0-20.8,16.9-37.9,37.7-37.8
+              c6.6,0,11.9-5.3,11.9-11.9C483.2,19,477.8,13.6,471.3,13.6"></path>
+        </svg>   </span></div>         
+					<div class="col-sm-3 footer-links"  data-aos="fade-up" data-aos-delay="400">
+					   <a href="#" >CAREERS</a>   
+                       <a href="#" >CULTURE</a>   
+                       <a href="#" >NEWSROOM</a>   
+                       <a href="#" >BLOG</a>   
+                       <a href="#" >EVENTS</a>   
+                       <a href="#" >CONTACT US</a> 
+                    </div>
+					<div class="col-sm-4 footer-links"  data-aos="fade-up" data-aos-delay="600">
+					   <a  href="#" >DATA</a>   
+                       <a  href="#" >PARTNERS</a>   
+                       <a  href="#" >PRIVACY</a>   
+                       <span>CONNECT</span>   
+                       <p class="social-links" style="margin-top:12px;">
+                       <a target="_blank" href="#"><i class="ion-social-facebook" style="left:17px;"></i></a>
+						<a target="_blank" href="#"><i class="ion-social-twitter"></i></a>
+                           
+						   <a target="_blank" href="#"><i class="ion-social-linkedin-outline" ></i></a>
+						</p>
+                       </div>
+                      <div class="col-sm-5 life"  data-aos="fade-up" data-aos-delay="800">
+                      <span style="display: block; margin-top: 5px;">LIFE @ NEAR</span><br/>
+                       
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun1" alt="life @ Near" /></a>
+													
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun2" alt="life @ Near" /></a>
+							
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun3" alt="life @ Near" /></a>
+							
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun4" alt="life @ Near" /></a>
+						
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun5" alt="life @ Near" /></a>
+						
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun6" alt="life @ Near" /></a>
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun1" alt="life @ Near" /></a>
+													
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun2" alt="life @ Near" /></a>
+							
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun3" alt="life @ Near" /></a>
+							
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun4" alt="life @ Near" /></a>
+						
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun5" alt="life @ Near" /></a>
+						
+								<a target="_blank" href="#" title="life @ Near"><img src="<?= IMAGE ?>bb.png" width="80" height="80" class=" fun6" alt="life @ Near" /></a>
+						
+                       </div>
+					 
+				</div>
+				<div class="row">
+					<div class="col-sm-12" >
+						<hr style="border-top:1px solid rgba(0,0,0,0.5); margin-bottom:10px; padding-bottom:10px;"/>
+					</div>
+				</div>
+				<div class="row">
+					
+					 <div class="col-md-10 copyrights"  style="margin-top: 3px;">Copyright © 2016 - 2017 Near Pte. Ltd | <a href="#" target="_blank">Privacy Policy</a> | <a href="#" target="_blank"> Sitemap</a></div>
+					 <div class="col-md-2 truste">
+					<div><a href="//privacy.truste.com/privacy-seal/validation?rid=79c90252-7a59-4f23-8681-9180af7a58de" target="_blank"><img style="border: none" src="//privacy-policy.truste.com/privacy-seal/seal?rid=79c90252-7a59-4f23-8681-9180af7a58de" alt="TRUSTe"/></a></div>
+			</div>
+				</div>
+			</div>
+            
+				
+		</footer>
