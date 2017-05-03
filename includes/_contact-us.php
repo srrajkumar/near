9cc79c97dd069dae52289f6535043040310152af
Rajkumar S R <rajkumar@near.co>
2017-05-03 19:35:00 +0530

    Updated Get in touch button

--- includes/_contact-us.php
+++ includes/_contact-us.php
@@ -2,7 +2,7 @@
      <div class="container spacing_margin text-center" >
      
     	<h1 class="bold" data-aos="fade-up" data-aos-delay="200">CONTACT US</h1>
-    	<p style="margin-bottom:0px;" data-aos="flip-left" data-aos-easing="ease-in-back" data-aos-delay="400"><button type="button" class="btn btn-danger">Get in Touch</button></p>
+    	<p style="margin-bottom:0px;" data-aos="flip-left" data-aos-easing="ease-in-back" data-aos-delay="400"><a class="effect effect-1" href="#" title="Get in Touch">Get in Touch</a></p>
     	<span class="line" data-aos="flip-left" data-aos-easing="ease-in-back" data-aos-delay="600"></span>
     	<div class="row">
     	<div class="col-sm-12" data-aos="fade-up" data-aos-delay="600">
