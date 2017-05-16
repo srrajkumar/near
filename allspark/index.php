90c61bed905ee77428d6843497faf80947a4f0f2
Rajkumar S R <rajkumar@near.co>
2017-05-16 13:05:42 +0530

    Updated Brandlogos height fix and Updated metatag error in Homepage, Allspark page

--- allspark/index.php
+++ allspark/index.php
@@ -4,10 +4,11 @@ $meta_desc="";
 $meta_keywords="";
 ?>
 <!doctype html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<!--[if IE 7]><html lang="en" class="ie7"><![endif]-->
+<!--[if IE 8]><html lang="en" class="ie8"><![endif]-->
+<!--[if IE 9]><html lang="en" class="ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><html lang="en"><![endif]-->
+<!--[if !IE]><html lang="en-US"><![endif]-->
 
 <html>
 <head>
