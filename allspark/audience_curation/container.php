217e9707045d4b40cd0328055dee5a0e9dd37c9f
cooolkiran12 <cooolkiran@gmail.com>
2017-05-17 17:05:39 +0530

    allspark subpages

---
+++ allspark/audience_curation/container.php
@@ -0,0 +1,71 @@
+<?php include('_banner.php'); ?>
+<div class="container container--fixed top-spacer" style="padding: 100px 0">
+            <div class="row" style="margin: 0;">
+                <div class="col col-md-3">
+
+                    <!-- Side Menu -->
+                    
+
+<div class="side-menu">
+    <ul class="list-group sidebar-nav-v1" id="sidebar-nav">
+    	<li class="list-group-item list-toggle"><a href="#"></a></li>
+		<li class="list-group-item list-toggle active">
+			<a href="#" target="_self">USE CASES</a>
+			<ul class="list-group sidebar-nav-v1" id="sidebar-nav">
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self" class="active">Audience Curation</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Media Planning</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Out-of-Home Analytics</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Offline attribution</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Consumer research</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Omni-Channel Experience</a>
+				</li>
+			</ul>
+		</li>
+		<li class="list-group-item list-toggle"><a href="#"></a></li>
+		<li class="list-group-item list-toggle">
+			<a href="#" target="_self">VERTICAL</a>
+			<ul class="list-group sidebar-nav-v1" id="sidebar-nav">
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self" class="active">Audience Curation</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Media Planning</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Out-of-Home Analytics</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Offline attribution</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Consumer research</a>
+				</li>
+				<li class="list-group-item list-toggle">
+					<a href="#" target="_self">Omni-Channel Experience</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>
+</div>
+                <div class="col col-md-9 custom-banner">
+                <div class="col col-md-12">
+                	<div class="col-md-4">
+                	<div class="col-md-4">
+                	<div class="col-md-4">
+                </div>
+                    <img src="<?php IMAGE('allspark/banner.png') ?>" style="width: 100%; height: auto;">
+        </div>
+    </div>
+</div>
