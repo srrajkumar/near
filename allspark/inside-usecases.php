69db1158abc12e6a43a34ad6368cacb96966cf2a
cooolkiran12 <cooolkiran@gmail.com>
2017-05-20 10:27:48 +0530

    data page2

---
+++ allspark/inside-usecases.php
@@ -0,0 +1,194 @@
+<div class="row submenu-tiles perspective" style="margin: 0;">
+                    <h3>Inside Use Cases</h3>
+                    <div class="space" style="width: 100%"></div>
+                    <div class="col-md-4" data-aos="3dflip" data-aos-delay="200">
+                        <img src="<?php IMAGE('allspark/img-1.png') ?>" alt="High Power" class="sq-ratio">
+                        <span class="tile__content">
+                            <h4>Audience Curation</h4>
+                            <hr />
+                            <p class="description">
+                                Enterprises, Brands and Agencies can use Allspark to curate their audience of choice in real-time
+                            </p>
+                        </span>
+                            <a href="#" class="custom_btn">
+                                <span class="btn__text">
+                                    <span class="btn__text-inner">Explore</span>
+                                </span>            
+                                <div class="icon">
+                                    <svg class="" viewBox="0 0 18 7" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M14,3.09H0" fill="#fff"></path>
+                                        <polyline class="color_change" points="11.34 5.83 13.97 3.09 11.34 0.35" fill="#fff"></polyline>
+                                    </svg>
+                                </div>
+                            </a>
+                   </div>
+
+                    <div class="col-md-4" data-aos="3dflip" data-aos-delay="400">
+                   <a href="#" target="_self">
+                    <span class="button-icon-animate " style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;">
+                    <svg class="button-icon-animate__circle " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+                        <g class="st0" style="opacity:0.4;">
+                            <path class="st1" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,1.5c12.9,0,23.5,10.6,23.5,23.5S37.9,48.5,25,48.5S1.5,37.9,1.5,25S12.1,1.5,25,1.5 M25,0.5
+                            C11.5,0.5,0.5,11.5,0.5,25s11,24.5,24.5,24.5s24.5-11,24.5-24.5S38.5,0.5,25,0.5L25,0.5z"></path>
+                        </g>
+                        <g>
+                            <circle class="st1 button-icon-animate__circle--inner-circle" cx="25" cy="25" r="24.5" style="fill: rgb(255, 255, 255); transform: matrix(0, 0, 0, 0, 0.5, 0.5); visibility: visible; transform-origin: 0px 0px 0px;" data-svg-origin="25 25"></circle>
+                        </g>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--left" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="transform: matrix(-1, 0, 0, -1, 0, 0); " xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--right " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="" xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>        
+                    <span class="wobble"  style="animation-delay:0s;"><i class="icon ion-arrow-right-c"></i></span>
+                </span>
+                        <img src="<?php IMAGE('allspark/img-1.png') ?>" alt="High Power" class="sq-ratio">
+                        <span class="tile__content">
+                            <h4>Media Planning</h4>
+                            <p class="description">
+                                Brands and Agencies can use Allspark for Media Planning by
+                            </p>
+                            </span>
+                        </a>
+                        
+                    </div>
+
+
+                    <div class="col-md-4" data-aos="3dflip" data-aos-delay="600">
+                    <a href="#" target="_self">
+                    <span class="button-icon-animate " style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;">
+                    <svg class="button-icon-animate__circle " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+                        <g class="st0" style="opacity:0.4;">
+                            <path class="st1" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,1.5c12.9,0,23.5,10.6,23.5,23.5S37.9,48.5,25,48.5S1.5,37.9,1.5,25S12.1,1.5,25,1.5 M25,0.5
+                            C11.5,0.5,0.5,11.5,0.5,25s11,24.5,24.5,24.5s24.5-11,24.5-24.5S38.5,0.5,25,0.5L25,0.5z"></path>
+                        </g>
+                        <g>
+                            <circle class="st1 button-icon-animate__circle--inner-circle" cx="25" cy="25" r="24.5" style="fill: rgb(255, 255, 255); transform: matrix(0, 0, 0, 0, 0.5, 0.5); visibility: visible; transform-origin: 0px 0px 0px;" data-svg-origin="25 25"></circle>
+                        </g>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--left" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="transform: matrix(-1, 0, 0, -1, 0, 0); " xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--right " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="" xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>        
+                    <span class="wobble"  style="animation-delay:0s;"><i class="icon ion-arrow-right-c"></i></span>
+                </span>
+                        <img src="<?php IMAGE('allspark/img-1.png') ?>" alt="High Power" class="sq-ratio">
+                        <span class="tile__content">
+                            <h4>Out-of-Home Analytics</h4>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            </p>
+                            </span>
+                        </a>
+                        
+                    </div>
+
+
+                    <div class="col-md-4" data-aos="3dflip" data-aos-delay="700">
+                    <a href="#" target="_self">
+                    <span class="button-icon-animate " style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;">
+                    <svg class="button-icon-animate__circle " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+                        <g class="st0" style="opacity:0.4;">
+                            <path class="st1" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,1.5c12.9,0,23.5,10.6,23.5,23.5S37.9,48.5,25,48.5S1.5,37.9,1.5,25S12.1,1.5,25,1.5 M25,0.5
+                            C11.5,0.5,0.5,11.5,0.5,25s11,24.5,24.5,24.5s24.5-11,24.5-24.5S38.5,0.5,25,0.5L25,0.5z"></path>
+                        </g>
+                        <g>
+                            <circle class="st1 button-icon-animate__circle--inner-circle" cx="25" cy="25" r="24.5" style="fill: rgb(255, 255, 255); transform: matrix(0, 0, 0, 0, 0.5, 0.5); visibility: visible; transform-origin: 0px 0px 0px;" data-svg-origin="25 25"></circle>
+                        </g>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--left" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="transform: matrix(-1, 0, 0, -1, 0, 0); " xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--right " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="" xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>        
+                    <span class="wobble"  style="animation-delay:0s;"><i class="icon ion-arrow-right-c"></i></span>
+                </span>
+                        <img src="<?php IMAGE('allspark/img-1.png') ?>" alt="High Power" class="sq-ratio">
+                        <span class="tile__content">
+                            <h4>Offline attribution</h4>
+                            <p class="description">
+                                Enterprises, Brands, Agencies and Research Organisations can use Allspark to measure offline attribution for
+                            </p>
+                            </span>
+                        </a>
+                        
+                    </div>
+
+
+                    <div class="col-md-4" data-aos="3dflip" data-aos-delay="800">
+                    <a href="#" target="_self">
+                    <span class="button-icon-animate " style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;">
+                    <svg class="button-icon-animate__circle " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+                        <g class="st0" style="opacity:0.4;">
+                            <path class="st1" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,1.5c12.9,0,23.5,10.6,23.5,23.5S37.9,48.5,25,48.5S1.5,37.9,1.5,25S12.1,1.5,25,1.5 M25,0.5
+                            C11.5,0.5,0.5,11.5,0.5,25s11,24.5,24.5,24.5s24.5-11,24.5-24.5S38.5,0.5,25,0.5L25,0.5z"></path>
+                        </g>
+                        <g>
+                            <circle class="st1 button-icon-animate__circle--inner-circle" cx="25" cy="25" r="24.5" style="fill: rgb(255, 255, 255); transform: matrix(0, 0, 0, 0, 0.5, 0.5); visibility: visible; transform-origin: 0px 0px 0px;" data-svg-origin="25 25"></circle>
+                        </g>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--left" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="transform: matrix(-1, 0, 0, -1, 0, 0); " xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--right " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="" xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>        
+                    <span class="wobble"  style="animation-delay:0s;"><i class="icon ion-arrow-right-c"></i></span>
+                </span>
+                        <img src="<?php IMAGE('allspark/img-1.png') ?>" alt="High Power" class="sq-ratio">
+                        <span class="tile__content">
+                            <h4>Consumer research</h4>
+                            <p class="description">
+                                Enterprises, Brands and Research organisations can use Allspark to
+                            </p>
+                            </span>
+                        </a>
+                        
+                    </div>
+
+
+                    <div class="col-md-4" data-aos="3dflip" data-aos-delay="900">
+                   <a href="#" target="_self">
+                    <span class="button-icon-animate " style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;">
+                    <svg class="button-icon-animate__circle " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+                        <g class="st0" style="opacity:0.4;">
+                            <path class="st1" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,1.5c12.9,0,23.5,10.6,23.5,23.5S37.9,48.5,25,48.5S1.5,37.9,1.5,25S12.1,1.5,25,1.5 M25,0.5
+                            C11.5,0.5,0.5,11.5,0.5,25s11,24.5,24.5,24.5s24.5-11,24.5-24.5S38.5,0.5,25,0.5L25,0.5z"></path>
+                        </g>
+                        <g>
+                            <circle class="st1 button-icon-animate__circle--inner-circle" cx="25" cy="25" r="24.5" style="fill: rgb(255, 255, 255); transform: matrix(0, 0, 0, 0, 0.5, 0.5); visibility: visible; transform-origin: 0px 0px 0px;" data-svg-origin="25 25"></circle>
+                        </g>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--left" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="transform: matrix(-1, 0, 0, -1, 0, 0); " xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>
+
+                    <svg class="button-icon-animate__circle button-icon-animate__circle--right " version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="" xml:space="preserve">
+                        <path class="st0" style="fill:none;stroke:#FFFFFF;stroke-miterlimit:10;" d="M25,0.5c13.5,0,24.5,11,24.5,24.5S38.5,49.5,25,49.5"></path>
+                    </svg>        
+                    <span class="wobble"  style="animation-delay:0s;"><i class="icon ion-arrow-right-c"></i></span>
+                </span>
+                        <img src="<?php IMAGE('allspark/img-1.png') ?>" alt="High Power" class="sq-ratio">
+                        <span class="tile__content">
+                            <h4>Omni-Channel Experience</h4>
+                            <p class="description">
+                                Enterprises, Brands and Agencies can use Allspark for providing an omni-channel customer experience by
+                            </p>
+                            </span>
+                        </a>
+                        
+                    </div>
+                </div>
