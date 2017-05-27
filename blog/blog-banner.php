c23342459aa43c25ac678975160d040d3b088bcf
cooolkiran12 <cooolkiran@gmail.com>
2017-05-28 03:35:50 +0530

    blog banner style fix

--- blog/blog-banner.php
+++ blog/blog-banner.php
@@ -3,14 +3,14 @@
   <div class="container aboutus banner">
    <div class="row">
       <div class="col-md-12" style="text-align: center;">
-       <h1 class="blog-head"><span>Corporate Blog</span><a href="<?php echo SITE_URL; ?>engineering/" class="blog-link button2 button2--primary button2--rounded">Tech Blog</a></h1>
+       <h1 class="blog-head"><span style="display: block;">Corporate Blog</span><a href="<?php echo SITE_URL; ?>engineering/" class="blog-link button2 button2--primary button2--rounded" style=" font-size: 16px; line-height: 25px;">Tech Blog</a></h1>
         <!--<hr />
         <h2>Content</h2>
         <p>Content</p>-->
       </div>
    </div>
   </div>
-  <div class="banner-space container allspark-space"></div>
+  <div class="banner-space container allspark-space" style="    height: 60px;"></div>
   <div class="mouse"></div>
 
 </div>
