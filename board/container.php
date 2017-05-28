21fabb4ba4fc5dbe0fb649a5eb219382b394631c
cooolkiran12 <cooolkiran@gmail.com>
2017-05-28 14:24:43 +0530

    board page

---
+++ board/container.php
@@ -0,0 +1,47 @@
+<?php include('_banner.php'); ?>
+<div class="container board  " style="margin-bottom:50px;">
+<div class="space"></div>
+
+<div class="row">
+      <div class=" col-md-6">
+         <div class="col-md-12 margin-bottom" style="padding:0">
+         <div style="overflow: hidden">
+          <img class="img-circle" src="<?php IMAGE('bb.png'); ?>" style="background:url(<?php IMAGE('theboard.png'); ?>); background-position:0 364px;background-size:100% auto; " alt="Mohit Bhatnagar" width="120" height="120">
+          <h4 style="  margin-top: 41px;">Mohit Bhatnagar</h4> <span class="designation">Managing Director,<br> Sequoia Capital</span>
+          </div>
+          <hr class="mar" style="  margin-top: 20px;">
+          <p>Mohit is a Managing Director with Sequoia Capital and serves on the board of Comviva, Ujjivan Microfinance, IMI Mobile, Ideacts, Knowlarity, Micromax Mobile, One Assist and Citrus Pay. Prior to joining Sequoia Capital...</p>
+          <a  href="https://www.sequoiacap.com/india/people/mohit-bhatnagar/" target="_blank" class="btn-flat"> Read More</a><hr>
+        </div></div>
+      <div class="col-md-6">
+         <div class="col-md-12 margin-bottom" style="padding:0">
+         <div style="overflow: hidden">
+          <img class="img-circle" src="<?php IMAGE('bb.png'); ?>" style="background:url(<?php IMAGE('theboard.png'); ?>) ; background-position:0 117px;background-size:100% auto;" alt="Avneet Kochar" width="120" height="120">
+          <h4 style="  margin-top: 41px;">Avneet Kochar</h4><span class="designation">Regional Advisor,<br> JPM Private Equity Group</span>
+          </div>
+          <hr class="mar" style="  margin-top: 20px;">
+          <p>Avneet currently serves as private markets advisor for JPM PEG in New Delhi, India. He moved back to New Delhi from the US in 2012. Previously, he served as Director at the AT&T Investment Management Corporation...</p>
+          <a  href="https://am.jpmorgan.com/no/institutional/private-equity-group-professionals" target="_blank" class="btn-flat"> Read More</a><hr>
+        </div></div><!-- /.col-sm-6 -->
+      <div class="col-md-6">
+         <div class="col-md-12" style="padding:0">
+         <div style="overflow: hidden">
+          <img class="img-circle" src="<?php IMAGE('bb.png'); ?>" style="background:url(<?php IMAGE('theboard.png'); ?>) ; background-position:0 485px;background-size:100% auto;" alt="Mark Sherman" width="120" height="120">
+          <h4 style="  margin-top: 41px;">Mark Sherman</h4><span class="designation">Managing Director,<br> Telstra Ventures</span>
+          </div>
+          <hr class="mar" style="  margin-top: 20px;">
+          <p>Mark joined Telstra Ventures as a Managing Director in 2012. Previously, he was a General Partner at Hidden Lion Partners, an emerging growth equity firm focused on technology-enabled services...</p>
+          <a  href="http://www.telstra.com.au/ventures/people/" target="_blank" class="btn-flat">Read More</a><hr>
+        </div></div><!-- /.col-sm-6 -->
+      <div class="col-md-6">
+         <div class="col-md-12 margin-bottom" style="padding:0">
+         <div style="overflow: hidden">
+          <img class="img-circle" src="<?php IMAGE('bb.png'); ?>" style="background:url(<?php IMAGE('theboard.png'); ?>) ; background-position:0 0px;background-size:100% auto;" alt="Anil Mathews" width="120" height="120">
+          <h4 style="  margin-top: 41px;">Anil Mathews</h4><span class="designation">Founder & CEO,<br> Near</span>
+          </div>
+          <hr class="mar" style="  margin-top: 20px;">
+          <p>Anil is the Founder and CEO of Near. Anil built Near with a technology-centric approach into one of the largest location intelligence platform. Anil has spent the past 12 years as a serial entrepreneur.</p>
+         <hr>
+        </div></div><!-- /.col-sm-6 -->
+  </div>
+    </div><div class="space"></div>
